9774071d6a3bf9c2e930752b0aacf8dceb9eae3b
jury <jury8774@gmail>
2025-10-29 22:26:33 +0300

    initial commit

---
+++ 11.py
@@ -0,0 +1,10 @@
+import numpy as np
+#print(np.version)
+
+ppp = {
+    123,
+    123,
+}
+
+if True:
+    print(ppp)
---
+++ guess_number.py
@@ -0,0 +1,19 @@
+import numpy as np
+#print(np.version)
+    
+number = np.random.randint(1, 101) # загадываем число
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100\n"))
+
+    if predict_number > number:
+        print("Число должно быть меньше!\n")
+
+    elif predict_number < number:
+        print("Число должно быть больше!\n")
+
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток\n")
+        break # конец игры, выход из цикла
