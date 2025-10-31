65ce63eafd6d9a181cf0cea862d8c37097e18706
jury <jury8774@gmail>
2025-10-31 22:29:25 +0300

    подготавливаем для Jupiter, оборачиваем в функцию для импорта

--- guess_number.py
+++ guess_number.py
@@ -1,19 +1,24 @@
+"""Угадываем число самостоятельно
+"""
+
 import numpy as np
-#print(np.version)
-    
-number = np.random.randint(1, 101) # загадываем число
-count = 0
 
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100\n"))
 
-    if predict_number > number:
-        print("Число должно быть меньше!\n")
+def guess_number():
+    number = np.random.randint(1, 101) # загадываем число
+    count = 0
 
-    elif predict_number < number:
-        print("Число должно быть больше!\n")
+    while True:
+        count += 1
+        predict_number = int(input("Угадай число от 1 до 100\n"))
 
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток\n")
-        break # конец игры, выход из цикла
+        if predict_number > number:
+            print("Число должно быть меньше!\n")
+
+        elif predict_number < number:
+            print("Число должно быть больше!\n")
+
+        else:
+            print(f"Вы угадали число! Это число = {number}, за {count} попыток\n")
+            break # конец игры, выход из цикла
+    return
